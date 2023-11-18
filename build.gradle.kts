d2cbeb6eeb20d1e080dca7a297c8f2ce2b355cf7
Nikita Kamyshenko <nikita.kamyshenko@csssr.com>
2023-11-18 20:18:22 +0100

    Added spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("plugin.spring") version "1.7.22"
     kotlin("jvm") version "1.8.0"
     application
 
@@ -13,26 +16,40 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
     testImplementation(kotlin("test"))
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.2")
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.majorVersion
+    }
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
-}
-
 application {
-    mainClass.set("MainKt")
+    mainClass.set("org.example.ApplicationKt")
 }
 
 graalvmNative {
     binaries {
         named("main") {
             imageName.set("my-app")
-            mainClass.set("org.example.MainKt")
+            mainClass.set("org.example.ApplicationKt")
             buildArgs.add("-O4")
         }
         named("test") {
